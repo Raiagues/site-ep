0f526e97daf6dff81c700b05e557d57bd929f21f
Raiagues <Emilyrayannerodrigues@gmail.com>
2024-07-22 22:02:38 +0200

    fix position hidden content timeline

--- js/selecao.js
+++ js/selecao.js
@@ -6,10 +6,12 @@ document.addEventListener("DOMContentLoaded", function() {
   const defaultDescription = contentDisplay.innerHTML;
 
   let lastClickedId = null;
+  let lastClickedContent = null;
 
   cards.forEach(card => {
       card.addEventListener("click", function() {
           const targetId = this.getAttribute("data-target");
+          const targetContent = document.getElementById(targetId);
 
           if (window.innerWidth >= 500) {
 
@@ -26,7 +28,35 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
             }
         } else {
-          contentDisplay.style.display = "block";
+          if (lastClickedId === targetId) {
+          // Hide content if the same card is clicked again
+          if (lastClickedContent) {
+            lastClickedContent.style.display = "none";
+          }
+          lastClickedId = null;
+          lastClickedContent = null;
+          } else {
+            // Hide previous content if any
+            if (lastClickedContent) {
+              lastClickedContent.style.display = "none";
+            }
+
+            if (targetContent) {
+              const cardRect = this.getBoundingClientRect();
+              const containerRect = document.querySelector('.timeline-container').getBoundingClientRect();
+              
+              // Calculate top position relative to the container with fixed margin
+              targetContent.style.display = "block";
+              targetContent.style.position = "absolute"; // Position below the button
+              targetContent.style.top = `${cardRect.bottom - containerRect.top + 20}px`; 
+              targetContent.style.left = `${0}px`; // Align with the timeline container
+
+              
+
+              lastClickedContent = targetContent;
+              lastClickedId = targetId;
+            }
+          }
         }
       });
   });
