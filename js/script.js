9cdd2847bd6bc76d534fcdbc79ee7ba9fce22f52
Raiagues <Emilyrayannerodrigues@gmail.com>
2024-08-24 13:10:00 +0200

    frontend finished

--- js/script.js
+++ js/script.js
@@ -58,3 +58,32 @@ document.querySelectorAll('.btn-details').forEach(button => {
         }
     });
 });
+
+function sendEmail(event) {
+    event.preventDefault(); // Prevent the form from submitting the default way
+  
+    var form = document.getElementById("signup-form");
+    var params = {
+        from_name: form.name.value,
+        email_id: form.email.value,
+        message: form.message.value
+    };
+  
+    emailjs.send("service_ubzp5kb", "template_pez9ddl", params)
+        .then(function (res) {
+            showPopup("Sua mensagem foi enviada com sucesso!");
+            form.reset(); 
+        })
+        .catch(function (err) {
+            showPopup("Falha ao enviar a mensagem: " + err.text);
+        });
+  }
+  
+  function showPopup(message) {
+    document.getElementById("popup-message").textContent = message;
+    document.getElementById("popup").style.display = "flex";
+  }
+  
+  function closePopup() {
+    document.getElementById("popup").style.display = "none";
+  }
